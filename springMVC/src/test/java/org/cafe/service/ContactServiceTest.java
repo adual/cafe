ca83e4f289bf8dd99021d3bf33b80c51e52eae8e
pansanday <pansanday@gmail.com>
2014-01-15 15:12:28 +0800

    Pansanday>> Add a test case

--- springMVC/src/test/java/org/cafe/service/ContactServiceTest.java
+++ springMVC/src/test/java/org/cafe/service/ContactServiceTest.java
@@ -1,23 +1,29 @@
 package org.cafe.service;
 
 import junit.framework.TestCase;
-
 import org.cafe.domain.Contact;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContactServiceTest extends TestCase {
 
-	@Test
-	public void testSave() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"classpath*:spring/root-context.xml;spring/app-servlet/servlet-context.xml");
-		ContactService service = ctx.getBean("contactService", org.cafe.service.ContactServiceImpl.class);
-		Contact contact = new Contact();
-		contact.setFirstName("anders");
-		contact.setLastName("adu");
-		service.add(contact);
-		
-	}
+    private Logger logger = (Logger) LoggerFactory.getLogger(ContactServiceTest.class);
+
+    @Test
+    public void testSave() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:spring/root-context.xml;spring/app-servlet/servlet-context.xml");
+        ContactService service = ctx.getBean("contactService", org.cafe.service.ContactServiceImpl.class);
+        Contact contact = new Contact();
+        contact.setFirstName("anders");
+        contact.setLastName("adu");
+        service.add(contact);
+    }
+
+    @Test
+    public void testLogger() {
+        logger.info("Successfully!");
+    }
 }
