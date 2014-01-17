f978228fc4b5b99f32b4cf6a2a250436e2726cd6
adual <duzhiweibe@gmail.com>
2014-01-17 16:53:32 +0800

    a bad testcase

--- springMVC/src/test/java/org/cafe/service/ContactServiceTest.java
+++ springMVC/src/test/java/org/cafe/service/ContactServiceTest.java
@@ -1,29 +1,124 @@
 package org.cafe.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import junit.framework.TestCase;
+
 import org.cafe.domain.Contact;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class ContactServiceTest extends TestCase {
 
-    private Logger logger = (Logger) LoggerFactory.getLogger(ContactServiceTest.class);
+	private static Logger logger = (Logger) LoggerFactory
+			.getLogger(ContactServiceTest.class);
 
-    @Test
-    public void testSave() {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:spring/root-context.xml;spring/app-servlet/servlet-context.xml");
-        ContactService service = ctx.getBean("contactService", org.cafe.service.ContactServiceImpl.class);
-        Contact contact = new Contact();
-        contact.setFirstName("anders");
-        contact.setLastName("adu");
-        service.add(contact);
-    }
+	@Before
+	public void testLogger() {
+		logger.info("Successfully!");
+	}
 
-    @Test
-    public void testLogger() {
-        logger.info("Successfully!");
-    }
+	@Test
+	public void testSave() throws DataAccessException, Exception {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"C:/Users/IBM_ADMIN/git/cafe/springMVC/src/main/webapp/WEB-INF/spring/root-context.xml");
+		ContactService service = ctx.getBean("contactService",
+				org.cafe.service.ContactServiceImpl.class);
+		Contact contact = new Contact();
+		contact.setFirstName("anders");
+		contact.setLastName("adu");
+		// service.add(contact);
+		logger.info(service.getSysDate());
+
+	}
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx;
+		try {
+			ctx = new ClassPathXmlApplicationContext(
+					"classpath*:spring/root-context.xml");
+			System.out.println(ctx.containsBean("dataSource"));
+			 ContactService service = ctx.getBean("contactService",
+			 org.cafe.service.ContactServiceImpl.class);
+			Contact contact = (Contact) ctx.getBean("contact");
+			/*
+			 * Contact contact = new Contact(); contact.setFirstName("anders");
+			 * contact.setLastName("adu"); service.add(contact);
+			 */
+			logger.info(contact.getFirstName());
+			 logger.info(String.format("The database sys date %s", service.getSysDate()));
+
+			// testJdbc();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testJdbc() {
+		String url = "jdbc:mysql://127.0.0.1:3306/cafe";
+		String user = "root";
+		String pwd = "10086216";
+		String driverClass = "org.mariadb.jdbc.Driver";
+
+		String sql = "SELECT sysdate() as SYSDATE";
+
+		ResultSet res = null;
+		Statement st = null;
+		Connection conn = null;
+
+		try {
+			Class.forName(driverClass);
+
+			conn = DriverManager.getConnection(url, user, pwd);
+			st = conn.createStatement();
+			res = st.executeQuery(sql);
+			while (res.next()) {
+				String sysdate = res.getString("SYSDATE");
+				System.out.println(sysdate);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (res != null) {
+				try {
+					res.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (st != null) {
+				try {
+					res.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (conn != null) {
+				try {
+					res.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
