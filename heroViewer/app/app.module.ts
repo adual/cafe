55404e708ffe0639f7eed26380b5e2731f099bab
Anders <dzhiwei@cn.ibm.com>
2016-11-18 17:47:30 +0800

    add hero form component

--- heroViewer/app/app.module.ts
+++ heroViewer/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form.component'
 
 @NgModule({
   id: module.id,
