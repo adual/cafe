d19b02b45272742c7ab83f15cd8080d8dfe5f719
Anders <dzhiwei@cn.ibm.com>
2016-11-28 18:14:08 +0800

    add form component

--- heroViewer/app/app.module.ts
+++ heroViewer/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form.component'
+import { HeroFormComponent } from './hero-form.component';
+import { Logger } from './logger.service';
 
 @NgModule({
   id: module.id,
   imports: [ BrowserModule , FormsModule ],
+  providers: [ Logger ],
   declarations: [ AppComponent, HeroFormComponent ],
   bootstrap: [AppComponent ]
 })
--- heroViewer/app/hero-form.component.ts
+++ heroViewer/app/hero-form.component.ts
@@ -16,6 +16,8 @@ export class HeroFormComponent{
 
   onSubmit(){ this.submitted = true; }
 
+  newHero(){ this.model = new Hero(42,'',this.powers[0],''); }
+
   //TODO: remove this when we're done
   get diagnostic(){
     return JSON.stringify(this.model);
